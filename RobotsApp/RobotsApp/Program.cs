902ddf7407cd86a54ff95fa8b9c9f80dcd973ce1
Denis <den_vesok@mail.ru>
2016-12-29 05:39:41 +0300

    Add robots app.

---
+++ RobotsApp/RobotsApp/Program.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RobotsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ RobotsApp/RopotsAppTests/RopotAppTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RopotsApp
+{
+    [TestClass]
+    public class RopotAppTests
+    {
+        [TestClass]
+        public class MatrixTests
+        {
+            [TestMethod]
+            public void SimpleImposibleSystemTest()
+            {
+                bool[,] matrix = new bool[2, 2] { { true, true }, { true, true } };
+                bool[] robotPosition = new bool[2] { true, true };
+                Graph graph = new Graph(matrix, robotPosition.Length);
+                RobotsTask robots = new RobotsTask(graph, robotPosition);
+                Assert.IsFalse(robots.SystemStatus());
+            }
+
+            [TestMethod]
+            public void SimplePosibleSystemTest()
+            {
+                bool[,] matrix = new bool[3, 3] { { true, true, true }, { true, true, true }, { true, true, true } };
+                bool[] robotPosition = new bool[3] { true, false, true };
+                Graph graph = new Graph(matrix, robotPosition.Length);
+                RobotsTask robots = new RobotsTask(graph, robotPosition);
+                Assert.IsTrue(robots.SystemStatus());
+            }
+
+            [TestMethod]
+            public void NormalImposibleSystemTest()
+            {
+                bool[,] matrix = new bool[4, 4] { { true, false, true, true }, { false, true, true, false }, { true, true, true, false }, { true, false, false, true } };
+                bool[] robotPosition = new bool[4] { true, true, true, false };
+                Graph graph = new Graph(matrix, robotPosition.Length);
+                RobotsTask robots = new RobotsTask(graph, robotPosition);
+                Assert.IsFalse(robots.SystemStatus());
+            }
+
+            [TestMethod]
+            public void NormalPosibleSystemTest()
+            {
+                bool[,] matrix = new bool[4, 4] { { true, false, true, true }, { false, true, false, true }, { true, false, true, true }, { true, true, true, true } };
+                bool[] robotPosition = new bool[4] { true, true, true, false };
+                Graph graph = new Graph(matrix, robotPosition.Length);
+                RobotsTask robots = new RobotsTask(graph, robotPosition);
+                Assert.IsTrue(robots.SystemStatus());
+            }
+        }
+    }
+}

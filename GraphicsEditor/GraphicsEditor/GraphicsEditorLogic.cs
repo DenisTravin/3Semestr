43f938bbdac26558de2ee98eae044a2e6a13a07b
MSI\Denis <den_vesok@mail.ru>
2017-02-17 11:23:01 +0400

    Add my graph editor.

---
+++ GraphicsEditor/GraphicsEditor/GraphicsEditorLogic.cs
@@ -0,0 +1,130 @@
+﻿namespace GraphicsEditorNamespace
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Windows.Forms;
+    using System.Drawing;
+
+    /// <summary>
+    /// graphics editor main logic class
+    /// </summary>
+    public class GraphicsEditorLogic
+    {
+        private const int pxConst = 5;
+
+        private List<Line> lines;
+        
+
+        /// <summary>
+        /// class constructor
+        /// </summary>
+        public GraphicsEditorLogic()
+        {
+            lines = new List<Line>();
+        }
+
+        /// <summary>
+        /// get current logic state
+        /// </summary>
+        /// <returns>got state</returns>
+        public List<Line> GetState()
+        {
+            List<Line> outputList = new List<Line>();
+            foreach (var item in lines)
+            {
+                outputList.Add(item);
+            }
+            return outputList;
+        }
+
+        /// <summary>
+        /// set logic state
+        /// </summary>
+        /// <param name="givenList">given state</param>
+        public void SetState(List<Line> givenList)
+        {
+            lines = givenList;
+        }
+
+        /// <summary>
+        /// add new line
+        /// </summary>
+        /// <param name="newLine">added line</param>
+        public void AddLine(Line newLine)
+        {
+            lines.Add(newLine);
+        }
+
+        /// <summary>
+        /// remove line
+        /// </summary>
+        /// <param name="line">removed line</param>
+        private void RemoveLine(Line line)
+        {
+            lines.Remove(line);
+        }
+
+        /// <summary>
+        /// draw all lines
+        /// </summary>
+        /// <param name="e"></param>
+        public void Draw(PaintEventArgs e)
+        {
+            foreach (var item in lines)
+            {
+                item.Draw(e);
+            }
+        }
+
+        /// <summary>
+        /// Remove the nearest line to the given point
+        /// </summary>
+        /// <param name="point"> point </param>
+        /// <returns> 1 - if such line exists, 0 - otherwise </returns>
+        public bool RemoveLine(System.Drawing.PointF point)
+        {
+            foreach (var item in lines)
+            {
+                if (item.Distance(point) < pxConst)
+                {
+                    RemoveLine(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// calculatee distance between 2 points
+        /// </summary>
+        /// <param name="first">first point</param>
+        /// <param name="second">second point</param>
+        /// <returns>calculated distance</returns>
+        private double DistanceBetweenPoints(PointF first, PointF second) => Math.Sqrt(Math.Pow(first.X - second.X, 2) + Math.Pow(first.Y - second.Y, 2));
+
+        /// <summary>
+        /// find line that was dragged by user and return its not dragged end
+        /// </summary>
+        /// <param name="point">dragged point</param>
+        /// <returns>not dragged line end</returns>
+        public PointF FindNotDraggedLineEnd(PointF point)
+        {
+            foreach (var item in lines)
+            {
+                if (DistanceBetweenPoints(item.Start, point) < pxConst)
+                {
+                    var temp = item.End;
+                    RemoveLine(item);
+                    return temp;
+                }
+                else if (DistanceBetweenPoints(item.End, point) < pxConst)
+                {
+                    var temp = item.Start;
+                    RemoveLine(item);
+                    return temp;
+                }
+            }
+            return new PointF();
+        }
+    }
+}
---
+++ GraphicsEditor/GraphicsEditor/Line.cs
@@ -0,0 +1,79 @@
+﻿namespace GraphicsEditorNamespace
+{
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// line class
+    /// </summary>
+    public class Line
+    {
+        /// <summary>
+        /// line start point
+        /// </summary>
+        public PointF Start { get; set; }
+
+        /// <summary>
+        /// line end point
+        /// </summary>
+        public PointF End { get; set; }
+
+        /// <summary>
+        /// line constructor
+        /// </summary>
+        /// <param name="start">line start</param>
+        /// <param name="end">line end</param>
+        public Line(PointF start, PointF end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        /// <summary>
+        /// draw line in picture box
+        /// </summary>
+        /// <param name="e"></param>
+        public void Draw(PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(new Pen(Color.Black, 3), Start, End);
+        }
+
+        private double DistanceBetweenPoints(PointF first, PointF second) => Math.Sqrt(Math.Pow(first.X - second.X, 2) + Math.Pow(first.Y - second.Y, 2));
+
+        /// <summary>
+        /// distance between line and the given point
+        /// </summary>
+        /// <param name="point">given point</param>
+        /// <returns>counted distance</returns>
+        public double Distance(PointF point)
+        {
+            var maxX = Math.Max(Start.X, End.X);
+            var maxY = Math.Max(Start.Y, End.Y);
+            var minX = Math.Min(Start.X, End.X);
+            var minY = Math.Min(Start.Y, End.Y);
+
+            if (maxX < point.X && maxY < point.Y)
+            {
+                return Math.Min(DistanceBetweenPoints(Start, point), DistanceBetweenPoints(End, point));
+            }
+            else if (maxX > point.X && minY > point.Y)
+            {
+                return Math.Min(DistanceBetweenPoints(Start, point), DistanceBetweenPoints(End, point));
+            }
+            else if (minX > point.X && maxY < point.Y)
+            {
+                return Math.Min(DistanceBetweenPoints(Start, point), DistanceBetweenPoints(End, point));
+            }
+            else if (minX > point.X && minY > point.Y)
+            {
+                return Math.Min(DistanceBetweenPoints(Start, point), DistanceBetweenPoints(End, point));
+            }
+
+            double A = (Start.Y - End.Y);
+            double B = (End.X - Start.X);
+            double C = (End.Y - Start.Y) * Start.X + (Start.X - End.X) * Start.Y;
+            return Math.Abs(A * point.X + B * point.Y + C) / (Math.Sqrt(A * A + B * B));
+        }
+    }
+}
